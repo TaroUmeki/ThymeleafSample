f48123467d012d30ac4c83450cf9db07f8d1aba1
TaroUmeki <umeki.taro@gmail.com>
2025-08-02 11:23:18 +0900

    01~03終了

---
+++ src/main/java/com/example/demo/controller/ThymeleafController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class ThymeleafController {
+	
+	@GetMapping("show")
+	public String showView(Model model) {
+		
+		//[Model]にデータを格納する
+		model.addAttribute("name","太郎");
+		
+		//戻り値はビュー名を返す
+		return "main";
+	}
+}
